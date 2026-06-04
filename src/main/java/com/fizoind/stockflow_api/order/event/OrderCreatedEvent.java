726213a00222273fe0260747981b078c0bda2c04
Fazil <fadhilithesupreme4@gmail.com>
2026-06-04 20:50:49 +0300

    fix: order created event

--- src/main/java/com/fizoind/stockflow_api/order/event/OrderCreatedEvent.java
+++ src/main/java/com/fizoind/stockflow_api/order/event/OrderCreatedEvent.java
@@ -3,7 +3,7 @@ package com.fizoind.stockflow_api.order.event;
 import com.fizoind.stockflow_api.order.entity.CustomerOrder;
 
 public class OrderCreatedEvent {
-   private CustomerOrder customerOrder;
+   private final CustomerOrder customerOrder;
 
     public OrderCreatedEvent(CustomerOrder customerOrder) {
         this.customerOrder = customerOrder;
